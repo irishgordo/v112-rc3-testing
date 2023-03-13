b68e9754dc7d8a8256ece7450fea423caabe3e4a
Mike Russell <michael.russell@suse.com>
2023-03-12 18:51:30 -0700

    updating...

---
+++ RKE1-Test-Artifacts/write_dir_files_to_markdown.py
@@ -0,0 +1,20 @@
+import os
+
+def main():
+    pictures = os.listdir()
+    try:
+        os.remove('README.md')
+        print(f'removed old readme')
+    except OSError as ex:
+        pass
+    with open("README.md", "w") as readme:
+        for pic in pictures:
+            if pic != 'write_dir_files_to_markdown.py':
+                readme.write('\n')
+                sntzepic = pic.replace(' ', '\\ ')
+                mrkdwnpic = (f'![{pic}](./{sntzepic})')
+                readme.write(mrkdwnpic)
+                readme.write('\n')
+
+if __name__ == "__main__":
+    main()
